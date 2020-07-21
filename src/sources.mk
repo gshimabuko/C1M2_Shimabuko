4df519dcc77f1f09b4ebe6641867b15205f4a4a8
gshimabuko <gshimabuko@gmail.com>
2020-07-21 20:25:31 -0300

    Finished makefile

--- src/sources.mk
+++ src/sources.mk
@@ -11,21 +11,22 @@
 
 # Add your Source files to this variable
 ifeq ($(PLATFORM), MSP432)
-	SOURCES =   main.c \
-                memory.c \
-                interrupts_msp432p401r_gcc.c
-                startup_msp432p401r.c
-                system_msp432p401r.c
+	SOURCES = main.c \
+				memory.c \
+				interrupts_msp432p401r_gcc.c \
+                startup_msp432p401r_gcc.c \
+				system_msp432p401r.c
 else
 	SOURCES =   main.c \
-                memory.c
+				memory.c
 endif
 
 # Add your include paths to this variable
 ifeq ($(PLATFORM), MSP432)
-	INCLUDES =  -I ../include/CMIS \
-            ../include/common \
-            ../include/msp432
+	INCLUDES =  -I ../include/CMSIS \
+				-I ../include/common \
+				-I ../include/msp432 \
+                -I ../
 else
 	INCLUDES =  -I ../include/common
 endif
